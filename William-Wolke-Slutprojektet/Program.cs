d7f843f92545d7699c45f72ef186465933bffb93
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2020-03-09 12:04:06 +0100

    Första commit
    Började med att göra en 2d array. Skrev ut den och började arbeta med att man har en pekare så man lätt kan välja vart man ska skjuta vart man är en en array med 2 platser.

--- William-Wolke-Slutprojektet/Program.cs
+++ William-Wolke-Slutprojektet/Program.cs
@@ -10,7 +10,50 @@ namespace William_Wolke_Slutprojektet
     {
         static void Main(string[] args)
         {
+            string[,] board;
+            int[] currentPosition = { 0, 0 };
 
+            InitialBoard(out board);
+            //WHile loop eftersom att man inte vet hur många gånger 
+            while (1 == 1)
+            {
+                //FOr loop eftersom att den endast kommer köras ett visst antal gånger
+                for (int i = 0; i < 10; i++)
+                {
+                    //for loop eftersom att den alltid kommer köras 10 gånger
+                    for (int j = 0; j < 10; j++)
+                    {
+                        if (board[i, j] == board[currentPosition[0], currentPosition[1]])
+                        {
+                            board[i, j] = "[ * ]";
+                        }
+
+                        else { 
+                            board[i, j] = "[   ]"; 
+                        }
+
+                        Console.Write(board[i, j]);
+                    }
+                    Console.WriteLine();
+                    currentPosition[1] += 1;
+                }
+                Console.ReadLine();
+            }
+        }
+
+        private static string[,] InitialBoard(out string[,] board)
+        {
+            //En 10*10 2d array
+            board = new string[10, 10];
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    
+                }
+            }
+
+            return board;
         }
     }
 }
